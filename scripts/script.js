726ff4c9dc2703295366962b2e0e9d698a5043ed
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-15 15:40:09 +0200

    -Initial functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,23 @@
 const canvas = document.querySelector('.canvas');
-const ROWS = 16;
-const COLS = 16;
+const cells = document.querySelectorAll('.grid-cell');
+const ROWS = 32;
+const COLS = 32;
 
 function makeCanvas(rows, cols) {
     canvas.style.setProperty('--grid-columns', cols);
     canvas.style.setProperty('--grid-rows', rows);
-
+    
     for(let i = 0; i < (rows * cols); i++){
         const cell = document.createElement('div');
         cell.classList.add('grid-cell');
+        cell.addEventListener("mouseover", (e) => {
+          e.target.style.setProperty('background-color', 'black');
+        //   console.log(e.target);
+        });
         canvas.appendChild(cell);
     }
 }
 
 makeCanvas(ROWS, COLS);
+
+
