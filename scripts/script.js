247984ca883b2a6162cc56faf44729ef47f3dfb7
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-15 17:41:14 +0200

    -Add tooltip over range bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,26 @@
 const canvas = document.querySelector('.canvas');
 const cells = document.querySelectorAll('.grid-cell');
+const range = document.querySelector('#grid-size');
+const rangeOutput = document.querySelector('.range-output');
 const ROWS = 32;
 const COLS = 32;
+
+/**
+ * Helper function to remove all child nodes
+ * @param {Node} parent 
+ */
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
 function makeCanvas(rows, cols) {
     canvas.style.setProperty('--grid-columns', cols);
     canvas.style.setProperty('--grid-rows', rows);
+    removeAllChildNodes(canvas);
     
-    for(let i = 0; i < (rows * cols); i++){
+    for(let i = 0; i < (rows * cols); i++){        
         const cell = document.createElement('div');
         cell.classList.add('grid-cell');
         cell.addEventListener("mouseover", (e) => {
@@ -19,5 +32,36 @@ function makeCanvas(rows, cols) {
 }
 
 makeCanvas(ROWS, COLS);
+
+/**
+ * Range input controls:
+ * 
+ * input event is instantaneous and will cause an overhead
+ * if we apply canvas change here. 
+ * Instead we will output a div that output grid size.
+ */
+
+range.addEventListener('mousemove', (e) => {
+    console.log(e.clientX, e.clientY);
+    // rangeOutput.style.top = e.clientY - 20 + "px";
+    rangeOutput.style.left = e.clientX + 10 + "px";
+});
+
+range.addEventListener('mouseover', (e) => {
+    rangeOutput.style.display = 'block';
+});
+
+range.addEventListener('mouseleave', (e) => {
+    rangeOutput.style.display = 'none';
+})
+
+range.addEventListener('input', (e) => {    
+    rangeOutput.textContent = `${e.target.value}x${e.target.value}`;
+
+});
+
+range.addEventListener('change', (e) => {
+    makeCanvas(e.target.value, e.target.value);    
+})
 
 
